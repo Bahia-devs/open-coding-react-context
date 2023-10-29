1cf163c3ee9b4606325c332e6f750811081172e6
Levy Santiago <levy.santiago@bitfy.app>
2023-10-29 09:56:16 -0300

    feat: creating sections components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react"
+import { SectionAbout } from "./components/section-about"
+import { SectionContact } from "./components/section-contact"
+import { SectionContext } from "./components/section-context"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);
+  }, [])
+
   return (
     <>
-      <div>
-        <p>
-          hello!
-        </p>
-      </div>
+      <h1>Hello World!</h1>
+      <SectionAbout loading={loading} />
+      <SectionContext loading={loading} />
+      <SectionContact loading={loading} />
     </>
   )
 }
---
+++ src/components/section-about/index.tsx
@@ -0,0 +1,14 @@
+export function SectionAbout({ loading }: { loading: boolean }) {
+  if (loading) {
+    return <p>loading...</p>
+  }
+
+  return (
+    <div>
+      <h2>Section About</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis urna nisi, cursus id sollicitudin non, mattis non ex. Donec odio sem, fringilla nec aliquam non, condimentum sed libero. Curabitur molestie velit in nunc pulvinar fermentum. Sed maximus congue massa, in consequat est fermentum at. Aliquam accumsan sem sem, quis sagittis ipsum rutrum ut. Nulla eu nibh at massa iaculis lacinia. Praesent aliquam malesuada erat quis sodales. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac lorem vitae nulla placerat viverra non a risus. Aenean commodo laoreet ante sit amet sagittis. Sed consectetur velit fringilla consequat cursus. Donec et diam sollicitudin, consequat arcu vitae, sagittis ligula. Fusce vel ex iaculis, ultricies dolor sed, imperdiet turpis.
+      </p>
+    </div>
+  )
+}
